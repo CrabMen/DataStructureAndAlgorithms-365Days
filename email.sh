d77a4eba1fe80d87b1249168f36282e1e867b8ff
CrabMen <tobecrabman@163.com>
2020-04-26 14:15:21 +0800

    modify sh

--- email.sh
+++ email.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
+# Cannot create a new backup.
+# A previous backup already exists in refs/original/
+# Force overwriting the backup with -f
+# 如果之前曾经执行过 git filter-branch 命令,在 refs/original/ 会有一个备份,这个时候需要删除备份,然后执行接下来的操作即可
+git update-ref -d refs/original/refs/heads/master
+
 git filter-branch --env-filter '
 
-OLD_EMAIL="crabman@guoqiangdeMacBook-Pro-2.local"
+OLD_EMAIL="tobecrabman@163.com"
 CORRECT_NAME="CrabMen"
 CORRECT_EMAIL="tobecrabman@163.com"
 
