086fdb614b1f6172118f336f9f594a88baca4e06
CrabMen <tobecrabman@163.com>
2020-04-29 14:01:29 +0800

    _279. 完全平方数(对于贪心和BFS理解还不是很透彻,视频进度需要提速了)

--- Stack&Queue/_279. 完全平方数.java
+++ Stack&Queue/_279. 完全平方数.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。
  * 
@@ -13,9 +17,44 @@
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
- 
+/**
+ * 
+ * 
+ * 作者：LeetCode
+ * 链接：https://leetcode-cn.com/problems/perfect-squares/solution/wan-quan-ping-fang-shu-by-leetcode/
+ * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
+
 class Solution {
     public int numSquares(int n) {
 
+        ArrayList<Integer> square_nums = new ArrayList<Integer>();
+        for (int i = 1; i * i <= n; ++i) {
+            square_nums.add(i * i);
+        }
+
+        Set<Integer> queue = new HashSet<Integer>();
+        queue.add(n);
+
+        int level = 0;
+        while (queue.size() > 0) {
+            level += 1;
+            Set<Integer> next_queue = new HashSet<Integer>();
+
+            for (Integer remainder : queue) {
+             
+                for (Integer square : square_nums) {
+                    if (remainder.equals(square)) {
+                        return level;
+                    } else if (remainder < square) {
+                        break;
+                    } else {
+                        next_queue.add(remainder - square);
+                    }
+                }
+            }
+            queue = next_queue;
+        }
+        return level;
     }
 }
