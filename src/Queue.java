0422548a2f66e9121cc0fcb72cb35c8b0f824e2d
CrabMen <tobecrabman@163.com>
2020-05-06 14:08:47 +0800

    新增队列实现;调整Main.java中的测试代码

---
+++ src/Queue.java
@@ -0,0 +1,31 @@
+
+import list.LinkedList;
+import list.List;
+
+public class Queue<E> {
+	private List<E> list = new LinkedList<>();
+	
+	public int size() {
+		return list.size();
+	}
+
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+	
+	public void clear() {
+		list.clear();
+	}
+
+	public void enQueue(E element) {
+		list.add(element);
+	}
+
+	public E deQueue() {
+		return list.remove(0);
+	}
+
+	public E front() {
+		return list.get(0);
+	}
+}
