13cfa8d6b79eecf9e38898d8e9d0d1025cb028b2
CrabMen <tobecrabman@163.com>
2020-05-15 10:24:31 +0800

    _面试题62_圆圈中最后剩下的数字(约瑟夫环问题)

---
+++ Leetcode/Array&String/_14_最长公共前缀.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+
+    }
+}
---
+++ Offer/_面试题62_圆圈中最后剩下的数字.java
@@ -0,0 +1,51 @@
+/**
+ * 0,1,,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字。求出这个圆圈里剩下的最后一个数字。
+ * 
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: n = 5, m = 3
+ * 
+ * 输出: 3
+ * 
+ * 示例 2：
+ * 
+ * 输入: n = 10, m = 17 输出: 2  
+ * 
+ * 限制：
+ * 
+ * 1 <= n <= 10^5
+ * 
+ * 1 <= m <= 10^6
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ */
+
+/**
+ * Sweetiee 这么著名的约瑟夫环问题，是有数学解法的！
+ * 
+ * 作者：sweetieeyi
+ * 链接：https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/solution/javajie-jue-yue-se-fu-huan-wen-ti-gao-su-ni-wei-sh/
+ * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * 
+ */
+
+class Solution {
+    public int lastRemaining(int n, int m) {
+        int ans = 0;
+        // 最后一轮剩下2个人，所以从2开始反推
+        for (int i = 2; i <= n; i++) {
+            ans = (ans + m) % i;
+        }
+        return ans;
+    }
+}
