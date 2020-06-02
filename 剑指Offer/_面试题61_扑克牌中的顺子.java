c8187981f3dfa1f3b30f007a63a5b4901881af21
CrabMen <tobecrabman@163.com>
2020-06-02 18:47:31 +0800

    _面试题61_扑克牌中的顺子

---
+++ 剑指Offer/_面试题61_扑克牌中的顺子.java
@@ -0,0 +1,50 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 
+ * 从扑克牌中随机抽5张牌，判断是不是一个顺子，即这5张牌是不是连续的。2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0
+ * ，可以看成任意数字。A 不能视为 14。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3,4,5] 输出: True  
+ * 
+ * 示例 2:
+ * 
+ * 输入: [0,0,1,2,5] 输出: True  
+ * 
+ * 限制：
+ * 
+ * 数组长度为 5 
+ * 
+ * 数组的数取值为 [0, 13] .
+ * 
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/bu-ke-pai-zhong-de-shun-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 
+ */
+
+// 作者：jyd
+// 链接：https://leetcode-cn.com/problems/bu-ke-pai-zhong-de-shun-zi-lcof/solution/mian-shi-ti-61-bu-ke-pai-zhong-de-shun-zi-ji-he-se/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+class Solution {
+    public boolean isStraight(int[] nums) {
+
+        int min = 14, max = 0;
+        Set<Integer> repeat = new HashSet<>();
+        for (int num : nums) {
+            if (num == 0)
+                continue;
+            max = Math.max(max, num);
+            min = Math.min(min, num);
+            if (repeat.contains(num))
+                return false;
+            repeat.add(num);
+        }
+        return max - min < 5;
+    }
+}
