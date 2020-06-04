8c3ff02ee8ed76dd078a17c2572b77541188152a
CrabMen <tobecrabman@163.com>
2020-06-04 18:18:21 +0800

    _面试题47_礼物的最大价值(动态规划)

--- 剑指Offer/_面试题47_礼物的最大价值.java
+++ 剑指Offer/_面试题47_礼物的最大价值.java
@@ -1,8 +1,18 @@
-import java.util.Arrays;
 
 class Solution {
     public int maxValue(int[][] grid) {
+        if (grid == null)
+            return 0;
 
+        int rc = grid.length;
+        int cc = grid[0].length;
+        int[][] dp = new int[rc + 1][cc + 1];
 
+        for (int i = 1; i <= rc; i++) {
+            for (int j = 1; j <= cc; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+            }
+        }
+        return dp[rc][cc];
     }
 }
