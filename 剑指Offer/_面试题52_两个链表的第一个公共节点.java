df5da2b7de81ce5889996d24f90af54d7778a6cb
CrabMen <tobecrabman@163.com>
2020-06-04 11:59:28 +0800

    _面试题52_两个链表的第一个公共节点(HashMap)

--- 剑指Offer/_面试题52_两个链表的第一个公共节点.java
+++ 剑指Offer/_面试题52_两个链表的第一个公共节点.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * 如果两个链表没有交点，返回 null. 在返回结果后，两个链表仍须保持原有的结构。 可假定整个链表结构中没有循环。 程序尽量满足 O(n)
  * 时间复杂度，且仅用 O(1) 内存。 本题与主站 160
@@ -22,15 +24,36 @@ public class ListNode {
 
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return getIntersectionNode_doublePointer(headA, headB);
+    }
+
+    static ListNode getIntersectionNode_doublePointer(ListNode headA, ListNode headB) {
         ListNode pointerA = headA;
         ListNode pointerB = headB;
-
-        while (pointerA != pointerB ) {
+        // A+B 和B+A 路程一致
+        while (pointerA != pointerB) {
             pointerA = pointerA == null ? headB : pointerA.next;
             pointerB = pointerB == null ? headA : pointerB.next;
         }
 
         return pointerA;
+    }
+
+    static ListNode getIntersectionNode_map(ListNode headA, ListNode headB) {
+        ListNode nodeA = headA;
+        ListNode nodeB = headB;
+        HashMap<ListNode,ListNode> map = new HashMap<>();
+
+        while (nodeA != null) {
+            map.put(nodeA, nodeA.next);
+            nodeA = nodeA.next;
+        }
+        while (nodeB != null) {
+            if (map.containsKey(nodeB))
+                return nodeB;
+            nodeB = nodeB.next;
+        }
+        return null;
     }
 
 }
