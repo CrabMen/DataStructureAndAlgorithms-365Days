19b719630953603a83a20b41b9055608327607b4
CrabMen <tobecrabman@163.com>
2020-06-11 13:25:10 +0800

    KMP

--- 11-字符串/src/com/sequence/KMP.java
+++ 11-字符串/src/com/sequence/KMP.java
@@ -3,20 +3,24 @@ package com.sequence;
 public class KMP {
 
 	public static int indexOf(String text, String pattern) {
-		if (text == null || pattern == null) return -1;
+		if (text == null || pattern == null)
+			return -1;
+		if (text.length() == 0 || pattern.length() == 0)
+			return -1;
 		char[] textChars = text.toCharArray();
-		int tlen = textChars.length;
-		if (tlen == 0) return -1;
 		char[] patternChars = pattern.toCharArray();
+
+		int tlen = textChars.length;
 		int plen = patternChars.length;
-		if (plen == 0) return -1;
-		if (tlen < plen) return -1;
-		
+
+		if (tlen < plen)
+			return -1;
 		// next表
-		int[] next = next(pattern);
-		
+		int[] next = next1(pattern);
+
 		int pi = 0, ti = 0, lenDelta = tlen - plen;
 		while (pi < plen && ti - pi <= lenDelta) {
+			// pi = -1 时,pi++ 归零 ,ti往后移动一位
 			if (pi < 0 || textChars[ti] == patternChars[pi]) {
 				ti++;
 				pi++;
@@ -26,50 +30,59 @@ public class KMP {
 		}
 		return (pi == plen) ? (ti - pi) : -1;
 	}
-	
-	private static int[] next(String pattern) {
+
+	/**
+	 * next 数组各值的含义：
+	 * 
+	 * 代表当前字符之前的字符串中，相同真前缀后缀的最大长度。
+	 * 
+	 * 相当于当前字符串的真前缀后缀的“最大长度值” 整体向右移动一位，然后初始值赋为-1。
+	 * 
+	 */
+
+	private static int[] next1(String pattern) {
 		char[] chars = pattern.toCharArray();
 		int[] next = new int[chars.length];
-		
-		// n == next[i]
+
 		next[0] = -1;
 		int i = 0;
 		int n = -1;
+
+		// chars[n]表示前缀，chars[i]表示后缀
 		int iMax = chars.length - 1;
+
 		while (i < iMax) {
 			if (n < 0 || chars[i] == chars[n]) {
 				++i;
 				++n;
-
-				if (chars[i] == chars[n]) {
+				if (chars[i] == chars[n])
 					next[i] = next[n];
-				} else {
+				else
 					next[i] = n;
-				}
 			} else {
 				n = next[n];
 			}
 		}
 		return next;
 	}
-	
-	private static int[] next2(String pattern) {
+
+	private static int[] next0(String pattern) {
 		char[] chars = pattern.toCharArray();
 		int[] next = new int[chars.length];
-		
-		// n == next[i]
+
 		next[0] = -1;
-		int i = 0;
 		int n = -1;
+		int i = 0;
+		// chars[n]表示前缀，chars[i]表示后缀
 		int iMax = chars.length - 1;
+
 		while (i < iMax) {
-			if (n < 0 || chars[i] == chars[n]) {
+			if (n < 0 || chars[i] == chars[n])
 				next[++i] = ++n;
-			} else {
+			else
 				n = next[n];
-			}
 		}
 		return next;
 	}
-	
+
 }
