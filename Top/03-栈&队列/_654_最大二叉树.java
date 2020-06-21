fe2afc94292743d1ab03bbe388bd966e3fbc4592
CrabMen <tobecrabman@163.com>
2020-06-21 16:41:55 +0800

    扩充链表题目

--- Top/03-栈&队列/_654_最大二叉树.java
+++ Top/03-栈&队列/_654_最大二叉树.java
@@ -32,6 +32,7 @@
  *  
  */
 import java.util.Arrays;
+import java.util.Stack;
 
 class _654_最大二叉树 {
 
@@ -64,9 +65,56 @@ class _654_最大二叉树 {
     }
 
     /**
-     * 最大二叉树变种
+     * 
+     * 返回一个数组，数组里面存着最大二叉树中每个节点的父节点的索引(如果没有父节点，就存 -1 )
+     * 
+     * 当前元素第一个比他大的元素 就为当前元素的父节点 使用一个单调递减的栈,对元素左侧第一个比他大的数和右侧第一个比他大的数,然后去二者中最小值
+     * 
+     * 该代码必须可以默写出来,最高温度的题 与该题干思想类似,利用栈可求出左侧或右侧第一个比他大的值
      */
+
     public int[] parentIndexes(int[] nums) {
+        /*
+         * 1.扫描一遍所有的元素
+         * 
+         * 2.保持栈从栈底到栈顶是单调递减的
+         */
+
+        if (nums == null || nums.length == 0)
+            return null;
+
+        int[] lis = new int[nums.length];// 左侧元素下标数组
+        int[] ris = new int[nums.length];// 右侧元素下标数组
+        Stack<Integer> stack = new Stack();
+
+        for (int i = 0; i < nums.length; i++) {
+            lis[i] = -1;
+            ris[i] = -1;
+            while (!stack.isEmpty() && nums[i] > nums[stack.peek()]) {
+                ris[stack.pop()] = i;
+            }
+            if (!stack.isEmpty())
+                lis[i] = stack.peek();
+
+            stack.push(i);
+        }
+
+        int[] pis = new int[nums.length];
+        for (int i = 0; i < pis.length; i++) {
+            if (lis[i] == -1 && ris[i] == -1) {
+                // i位置的是根节点
+                pis[i] = -1;
+                continue;
+            }
+            if (lis[i] == -1 || nums[lis[i]] > nums[ris[i]]) {
+                pis[i] = ris[i];
+            }
+
+            if (ris[i] == -1 || nums[lis[i]] < nums[ris[i]]) {
+                pis[i] = lis[i];
+            }
+
+        }
         return new int[0];
     }
 
