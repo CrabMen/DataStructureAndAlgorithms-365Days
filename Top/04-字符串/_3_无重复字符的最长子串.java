9924410a989ead813b38854ce8280fdd69426911
CrabMen <tobecrabman@163.com>
2020-06-23 10:56:08 +0800

    滑动窗口相关题目

--- Top/04-字符串/_3_无重复字符的最长子串.java
+++ Top/04-字符串/_3_无重复字符的最长子串.java
@@ -46,8 +46,33 @@ class Solution {
     }
 
     /**
-     * 大佬的题解 使用滑动窗口已经max函数. 更容易理解
-    */
+     * 大佬的题解 使用滑动窗口结合max函数. 更容易理解,相似题目
+     * 
+     * 3. 无重复字符的最长子串
+     * 
+     * 30. 串联所有单词的子串
+     * 
+     * 76. 最小覆盖子串
+     * 
+     * 159. 至多包含两个不同字符的最长子串
+     * 
+     * 209. 长度最小的子数组
+     * 
+     * 239. 滑动窗口最大值
+     * 
+     * 340. 至多包含 K 个不同字符的最长子串
+     * 
+     * 567. 字符串的排列
+     * 
+     * 632. 最小区间
+     * 
+     * 727. 最小窗口子序列
+     * 
+     * 作者：powcai
+     * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/solution/hua-dong-chuang-kou-by-powcai/
+     * 来源：力扣（LeetCode） 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * 
+     */
     public int lengthOfLongestSubstring_slidingWindow(String s) {
         if (s == null || s.length() == 0)
             return 0;
@@ -56,11 +81,27 @@ class Solution {
         int li = 0, max = 0;
         for (int i = 0; i < chars.length; i++) {
             if (map.containsKey(chars[i]))
-                li = Math.max(li, map.get(chars[i])+1);
+                li = Math.max(li, map.get(chars[i]) + 1);
             map.put(chars[i], i);
             max = Math.max(max, i - li + 1);
         }
         return max;
+    }
+
+    public int lengthOfLongestSubstring_sl(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        char[] chars = s.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int li = 0, max = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (map.containsKey(chars[i]))
+                li = Math.max(li, map.get(chars[i]) + 1);
+            map.put(chars[i], i);
+            max = Math.max(max, i - li + 1);
+        }
+        return max;
+
     }
 
 }
