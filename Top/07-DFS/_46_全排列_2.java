bcdb426937011e1545cab5543ba621b8a73c4318
CrabMen <tobecrabman@163.com>
2020-06-30 20:30:24 +0800

    _46_全排列_2(待调试)

---
+++ Top/07-DFS/_46_全排列_2.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,2,3]
+输出:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/permutations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/permutations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 
+ */
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+class Solution {
+
+    private List<List<Integer>> list;
+    private int[] nums;
+    /** 用来保存每一层选择的数字 */
+    private int[] result;
+    /** 用来标记nums中的数字是否被使用过了 */
+    private boolean[] used;
+
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null)
+            return null;
+        list = new ArrayList<>();
+        if (nums.length == 0)
+            return list;
+        this.nums = nums;
+        result = new int[nums.length];
+        used = new boolean[nums.length];
+        dfs(0);
+        return list;
+    }
+
+    private void dfs(int idx) {
+        // 不能再往下搜索
+        if (idx == nums.length) {
+            List<Integer> resultList = new ArrayList<>();
+            for (int value : result) {
+                resultList.add(value);
+            }
+            list.add(resultList);
+            return;
+        }
+
+        // 枚举这一层所有可以做出的选择
+        for (int i = idx; i < nums.length; i++) {
+          swap(idx, i);
+          dfs(idx+1);
+          swap(idx, 1);
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
