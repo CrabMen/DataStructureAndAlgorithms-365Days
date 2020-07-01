48bed02cb7ea8380a9fb7c2999c8a7c7f7b49259
CrabMen <tobecrabman@163.com>
2020-07-01 19:01:44 +0800

    全排列

--- Top/07-DFS/_47_全排列II.java
+++ Top/07-DFS/_47_全排列II.java
@@ -1,43 +1,32 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 
- * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
- * 
- * 示例:
- * 
- * 输入: [1,2,3]
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+
+示例:
+
+输入: [1,1,2]
 输出:
 [
-  [1,2,3],
-  [1,3,2],
-  [2,1,3],
-  [2,3,1],
-  [3,1,2],
-  [3,2,1]
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
 ]
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/permutations
+链接：https://leetcode-cn.com/problems/permutations-ii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * 
- * 来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/permutations
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 
+ * 
  * 
  * 
  */
 
 /**
- * 
- * 
- * 
- * 
- * 
+ * 根据规律进行合理的剪枝
  */
 class Solution {
-
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         if (nums == null)
             return null;
         List<List<Integer>> list = new ArrayList<>();
@@ -58,10 +47,14 @@ class Solution {
         }
         // 枚举这一层所有可以做出的选择
         for (int i = idx; i < nums.length; i++) {
-            swap(nums,idx, i);
+            swap(nums, idx, i);
             dfs(idx + 1, nums, list);
-            swap(nums,idx, i);
+            swap(nums, idx, i);
         }
+    }
+
+    private isRepeat(int[]nums,int idx,int i){
+
     }
 
     private void swap(int[] nums, int i, int j) {
@@ -69,5 +62,4 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
 }
